11f34b20af8b15d2a3c029440fc865123c547d74
marriamdd <davitashvili.m3@gmail.com>
2024-06-12 15:55:13 +0400

    fix:quantity of tasks per column

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -9,15 +9,17 @@ function BoardPage() {
   if (boardName) {
     setBoardName(boardName);
   }
-  console.log(boardName, "endpoint");
+
   const currentPage = jsonBoards.boards.find((item) => item.name === boardName);
+
+  console.log(currentPage?.columns[1].name);
   console.log(currentPage);
   return (
     <div className="flex  gap-[1rem] ">
       {currentPage?.columns.map((item, index) => (
         <div>
           <div>
-            <h2>{`${currentPage?.columns[index].name} (${currentPage?.columns[index].name.length}) `}</h2>
+            <h2>{`${currentPage?.columns[index]?.name} (${currentPage?.columns[index]?.tasks.length}) `}</h2>
           </div>
           {item.tasks.map((m) => (
             <div className="w-[28.8rem] px-[1.6rem] py-[2.3rem] mb-[1rem]  rounded-[0.8rem] bg-contentLight dark:bg-contentDarkBG">
